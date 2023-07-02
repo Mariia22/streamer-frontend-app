f41f96220e32e92e884369ba7282bedaacc5eab1
Maria22 <m.ushakova22@gmail.com>
2023-07-02 18:17:08 +0200

    fix: socket

--- streamer-app/src/utils/socket.ts
+++ streamer-app/src/utils/socket.ts
@@ -2,4 +2,4 @@ import { io } from 'socket.io-client';
 import { baseURL } from './const';
 
 const URL = baseURL;
-export const socket = io(URL);
+export const socket = io(URL, { transports: ['websocket'] });
